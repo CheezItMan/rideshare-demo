0c95ef0358f56cf29cf947bc29682d483e36265e
CheezItMan <chris@adadevelopersacademy.org>
2019-04-12 08:32:33 -0700

    finish moving styles toward BEM

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def random_profile_pic(model)
+    if model.id % 4 == 0
+      return image_tag "https://placeimg.com/150/150/people", alt: model.name, class: model.class.to_s
+    elsif model.id % 4 == 1
+      return image_tag "http://www.placecage.com/150/150", alt: model.name, class: model.class.to_s
+    elsif model.id % 4 == 2
+      return image_tag "https://placebeard.it/150x150", alt: model.name, class: model.class.to_s
+    else
+      return image_tag "https://placeimg.com/150/150/people", alt: model.name, class: model.class.to_s
+    end
+  end
 end
