5ee195b333b8f3509b4154d04dd0218252da16d4
CheezItMan <chris@adadevelopersacademy.org>
2019-04-11 17:53:21 -0700

    finish passenger_test

--- test/models/passenger_test.rb
+++ test/models/passenger_test.rb
@@ -21,6 +21,7 @@ describe Passenger do
 
   describe "relationships" do
     it "will have trips" do
+      # Assert
       expect(passenger).must_respond_to :trips
 
       passenger.trips.each do |trip|
@@ -31,8 +32,10 @@ describe Passenger do
 
   describe "validations" do
     it "must have a name" do
+      # Arrange
       passenger.name = nil
 
+      # Assert
       expect(passenger.valid?).must_equal false
       expect(passenger.errors.keys).must_equal [:name]
     end
@@ -41,12 +44,15 @@ describe Passenger do
   describe "custom methods" do
     describe "can_request?" do
       it "will return true if they can request a ride" do
+        # Act-Assert
         expect(passenger.can_request?).must_equal true
       end
 
       it "will return false if they cannot" do
+        # Arrange
         passenger.request_ride!
 
+        # Act-Assert
         expect(passenger.can_request?).must_equal false
       end
     end
@@ -90,9 +96,39 @@ describe Passenger do
     end
 
     describe "current_trip" do
+      it "can get the passenger's current trip" do
+        # Arrange
+        passenger.request_ride!
+
+        # Act-Assert
+        current_trip = passenger.current_trip
+        expect(current_trip).must_be_instance_of Trip
+        expect(current_trip.rating).must_be_nil
+      end
+
+      it "will return nil if no ongoing trips" do
+        expect(passenger.current_trip).must_equal nil
+      end
     end
 
     describe "complete_trip!" do
+      it "will fail if no incomplete trips" do
+        expect {
+          passenger.complete_trip!(3)
+        }.must_raise Passenger::RideRequestError
+      end
+
+      it "can complete a trip" do
+        # Arrange
+        passenger.request_ride!
+
+        # Act
+        passenger.complete_trip!(3)
+
+        # Assert
+        current_trip = passenger.current_trip
+        expect(current_trip).must_be_nil
+      end
     end
   end
 end
